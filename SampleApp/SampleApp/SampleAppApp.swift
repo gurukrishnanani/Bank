93c4ecaf9c85455d26189f7d28c9ee2e7f0b069b
gurukrishnanani <nannasanigurukrishna@gmail.com>
2025-02-05 10:24:04 +0530

    SampleApp

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by admin on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
